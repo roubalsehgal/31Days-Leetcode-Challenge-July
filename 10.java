391c258ae55b76ae6a195d1e93f87623db7a892c
roubalsehgal <roubalsehgal2000@gmail.com>
2020-07-10 19:45:54 +0530

    Day10 - Flatten a Multilevel Doubly Linked List

---
+++ 10.java
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+	public Node flatten(Node head) {
+		Node cur = head;
+		Node tail = head;
+		Stack <Node> stack = new Stack <>();
+		while (cur != null) {
+			if (cur.child != null) {
+				Node child = cur.child;
+				if (cur.next != null) {
+					stack.push(cur.next);
+					cur.next.prev = null;
+				}
+				cur.next = child;
+				child.prev = cur;
+				cur.child = null;
+			}
+			tail = cur;
+			cur = cur.next;
+		}
+		while (!stack.isEmpty()) {
+			cur = stack.pop();
+			tail.next = cur;
+			cur.prev = tail;
+			while (cur != null) {
+				tail = cur;
+				cur = cur.next;
+			}
+		}
+		return head;
+	}
+}
